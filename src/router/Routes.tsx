947d82d18c7e7bcdcc243d2e5261e33ac579c993
Shpat Vata <135318451+Shpatvv@users.noreply.github.com>
2023-08-13 00:25:53 +0200

    uiFrom

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,13 +1,13 @@
 import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Box } from "@mui/material";
-
+import CreateCompany from '../pages/createCompany';
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "login", element: <Box>Login</Box> },
+      { path: "login", element: <Box><CreateCompany/></Box> },
       { path: "not-found", element: <Box>Not found</Box> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
--- src/theme.ts
+++ src/theme.ts
@@ -41,6 +41,7 @@ const theme = createTheme({
     },
     typography: {
         fontFamily: 'Roboto, sans-serif',
+ 
     },
 });
 
